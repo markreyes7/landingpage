43dd1173de940b8de394d1265162e96a58774cc7
Mark <mark.reyes7777@gmail.com>
2021-09-15 19:03:07 -0700

    commit before adjusting index.html

--- features.js
+++ features.js
@@ -6,58 +6,103 @@ var arr = new Array(15);
 
 const arr2 = new Array('p','p','p','l','a','a','a','y','t',
                         't','t','h','e0','e0','e0','p');
-
+let center = document.getElementById("center");
 let position = 0;
 
 let attempts = 0;
 
-
-/*const toChange = document.querySelector(".centerkeys");
-
-toChange.addEventListener("mouseover", function (e){
-    e.target.style.backgroundColor = 'blue';
-    console.log('im the bluey')
-}); */
+/*CREATE FUNCTION THAT WILL ADD HOVER EFFECT ON ALL OUR CENTERFLEX KEYS SO WE CAN TURN IT OFF
+DONE !!!!
+*/ 
 
 function checker(clickedID){
+    
     if (clickedID === arr2[position]){
         console.log("ready to be green!");
-        document.getElementById(clickedID).style.color = 'green';
-        position+=1;
+        arr[position] = arr2[p];
+        
+        
+        let num = 0;
+        let curr = document.getElementById(clickedID);
+        let id = setInterval(frameGreen, 10)
+        
+
+        function frameGreen(){
+            if (num === 30){
+                clearInterval(id);
+            }
+            else{
+                num++;
+                curr.style.boxShadow = '0px 0px '+ num + 'px 9px green'
+            }
+        }
+        
+
+     
+        
 
         if (position === arr2.length){
-            console.log("puzzle solved")
-            console.log("animation time!!!!!!")
+        /*transition should go here*/
             position = 0;
+            
         }
+        if (attempts > 5){
+        setTimeout(function(){document.getElementById(arr2[position]).style.boxShadow = ' 0 0 5px 2px green inset , 0px 0px '+ '12px ' +'12px ' + 'green'}, 850);
+        console.log("you reached me");
+        }
+        
+
+        else{
+            console.log("correct!")
+        }
+        position+=1;
     }
-    else if((clickedID !== arr2[position]) && (document.getElementById(clickedID).style.color === 'green')){
-        console.log("you played this already!!!!!!")
-    }
+
+    
     else if(clickedID !== arr2[position]){
+
+        let num = 0;
+        let curr = document.getElementById(clickedID);
+        let id = setInterval(frameRed, 30)
         
         console.log("current position is " + position);
-        document.getElementById(clickedID).style.color = 'red';
-        setTimeout(function(){document.getElementById(clickedID).style.color = '#acb1b1'}, 1500);
+        function frameRed(){
+            if (num === 30){
+                clearInterval(id);
+            }
+            else{
+
+                num++;
+                curr.style.boxShadow = ' 0px 0px '+ num + 'px ' +'9px ' + 'red';
+                setTimeout(function(){document.getElementById(clickedID).style.boxShadow = 'none'}, 810);
+            }
+        }
+        
         attempts +=1;
 
-        if(attempts > 8){
-            alert('do you need help learning the way?');  /// this will instead create a button element on screen where it will direct users to get help.
-            /*let mainDiv = document.createElement('div');
-            let sideDiv = document.createElement('div');
-            let otherSideDiv = document.createElement('div');
-            let grid = document.getElementById('grid');
-            mainDiv.appendChild(sideDiv);
-            mainDiv.appendChild(otherSideDiv);
-            mainDiv.classList.add('arrow');
-            sideDiv.classList.add('line');
-            otherSideDiv.classList.add('line');
-            grid.appendChild(mainDiv);*/
+        if(attempts > 5){
+            alert('FOLLOW THE GREEN');
+            let btn = document.createElement('div');
+            btn.textContent = 'Skip the secret';
+            btn.setAttribute('id', 'buttonStyle');
+            center.appendChild(btn);
+            setTimeout(function(){document.getElementById(arr2[position]).style.boxShadow = ' 0 0 5px 2px green inset ,0px 0px '+ '12px ' +'12px ' + 'green'}, 850);
+            /* add the help button*/
         }
         return;
     }
     
-    
+}
+
+function whiteHover(x){
+    x.style.boxShadow = '0 0 5px 2px rgb(131, 130, 130) inset, 0 0 10px 5px rgb(184, 181, 181)';
+    x.style.border = '1px transparent rgb(184, 181, 181)';
+
+}
+
+function removeHover(x){
+    x.style.boxShadow = 'none';
+    x.style.border = 'none';
 }
 
 function playE1(){
@@ -66,7 +111,7 @@ function playE1(){
     audio.currentTime = 0;
     audio.play();
     insert(uniqueNum);
-    isOkay();
+
     
 }
 function playE2(){
@@ -74,7 +119,7 @@ function playE2(){
     var audio = document.getElementById("e2");
     audio.currentTime = 0;
     audio.play();
-    isOkay();
+    
 }
 
 function playEFlat(){
@@ -83,7 +128,7 @@ function playEFlat(){
     audio.currentTime = 0;
     audio.play();
  
-    isOkay();
+    
 }
 
 function playEFlat2(){
@@ -91,7 +136,7 @@ function playEFlat2(){
     var audio = document.getElementById("eFlat2")
     audio.currentTime = 0;
     audio.play();
-    isOkay();
+   
     
 }
 
@@ -100,7 +145,7 @@ function playD(){
     var audio = document.getElementById("d");
     audio.currentTime = 0;
     audio.play();
-    isOkay();
+  
 }
 
 function playDFlat(){
@@ -108,7 +153,7 @@ function playDFlat(){
     var audio = document.getElementById("d2");
     audio.currentTime = 0;
     audio.play();
-    isOkay();
+   
 }
 
 function playA(){
@@ -116,7 +161,7 @@ function playA(){
     var audio = document.getElementById("aChord");
     audio.currentTime = 0;
     audio.play();
-    isOkay();
+   
 } //end of naive functions
 
 function insert(param){
@@ -146,24 +191,5 @@ function countRealNum(){
     console.log("printing current num " + num);
     return num;
 }
-/* uses the global array to check every iTh index. 
-Can be updated in the future to optimize performance on browser */
-function isOkay(){
-    var x = countRealNum();
-    for (i =0; i < x;i++){
-        if (arr[15] == arr2[15] ){
-            console.log("the user found the way!");
-           //ADD JS METHOD TO PRESENT THE NEW LANDING PAGE
-        }
-        else if(arr[i] != arr2[i]){
-            console.log("the user keys do not match the secret key at the index " + i);
-            arr.length =0;          //DON'T MAKE THE USER RESTART?
-            
-            }
-     else if(arr[i] == arr2[i]){
-            console.log("working good at index " + i);
-            //GET THE CURRENT ELEMENT CLICKED AND GIVE IT THE GREEN JS EFFECT
-        }
-    }
-}
+
 
