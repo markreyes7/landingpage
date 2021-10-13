3415bcc023918051d3caa3b06c5c7a3c0e5ddc0f
Mark <mark.reyes7777@gmail.com>
2021-10-12 20:16:20 -0700

    changes to scrollbar and main content

--- features-1.0.js
+++ features-1.0.js
@@ -1,7 +1,30 @@
-var start;
-var stopId;
-var progress;
-var toggle = false;
+let start, previousTimeStamp;
+
+let list = document.getElementById("list");
+let content = document.getElementById("main-content");
+let allList = list.querySelectorAll("li");
+let progressValue = document.getElementById("progress-value")
+
+
+let arr = [allList[0].dataset.location, allList[1].dataset.location, allList[2].dataset.location];
+let root = document.documentElement;
+var position = 0;   //position to be changed in keeping track of scrolling up or down.
+
+
+
+var scrollingDirection = 0; //idle
+var lastScroll = 9999;
+var scrollIdleTime = 300; // time interval that we consider a new scroll event
+let maxProgress = 99.9;
+let currProgress = 33.3
+
+
+
+
+
+list.addEventListener('wheel',wheel);
+
+
 
 function popup(){
     var mainDiv = document.getElementById("main");
@@ -38,25 +61,83 @@ function checkExistance(){
     return checker;
 }
 
-function displayProjects(timestamp){
-    var main = document.getElementById("main");
-    if (!start || progress > 1200) start = timestamp;
-    progress = (timestamp - start)/2;
-    main.style.right = progress + 'px';
 
-    if (progress == 600){
-        
+
+/*    -----------      from sketch    ----------------       */
+
+
+
+
+
+
+function wheel(e){
+    var delta = e.deltaY;
+    var timeNow = performance.now();
+    if (delta > 0 && ( scrollingDirection != 1 || timeNow > lastScroll + scrollIdleTime) ) {
+        scrollUp();
+        scrollingDirection = 1;
+    } else if (delta < 0 && ( scrollingDirection != 2 || timeNow > lastScroll + scrollIdleTime)) {
+        scrollDown();
+        scrollingDirection = 2;
     }
-    stopId = window.requestAnimationFrame(displayProjects);
+    lastScroll = timeNow;
 }
 
-function toggleAnimation(){
-    if (!toggle){
-        toggle = true;
-        window.requestAnimationFrame(displayProjects);
+function scrollUp(){           //function for going up
+    if (position == 0){
+        root.style.setProperty('--pixel-count', arr[position +1] + 'px');
+        list.style.animation = 'moveDown 1.4s';
+        list.style.transform = 'translateY(var(--pixel-count))';
+        console.log("uppies");
+        progression();
+        position +=1;
     }
-    else{
-        toggle = false;
-        cancelAnimationFrame(stopId);
+    else if(position == 1){
+        root.style.setProperty('--pixel-count', arr[position +1] + 'px');
+        list.style.animation = 'moveDownFurther 1.4s';
+        list.style.transform = 'translateY(var(--pixel-count))';
+        console.log("uppies part 2");
+        progression();
+        position +=1;
     }
+}
+
+function scrollDown(){
+    if (position == 2){
+        root.style.setProperty('--pixel-count', arr[position-1]+'px');
+        list.style.animation = 'moveUp 1.4s';
+        list.style.transform = 'translateY(var(--pixel-count))';
+        console.log("downies")
+        regression();
+        position -=1;
+    }
+    else if (position == 1){
+        root.style.setProperty('--pixel-count', arr[position-1]+'px');
+        list.style.animation = 'moveUpFurther 1.4s';
+        list.style.transform = 'translateY(var(--pixel-count))';
+        console.log("downies part 2");
+        regression();
+        position -=1;
+    }
+}
+
+function progression(){
+    currProgress += 33.3;
+    console.log('curr progress is ' + currProgress);
+    if(Math.round(currProgress) === Math.round(maxProgress) ){        //needs work. Math.round() is used since 33.3 + 66.6 == about 99.89.....
+        progressValue.style.height = 100 + '%'
+        return;
+
+    }
+    progressValue.style.height = currProgress + '%';
+}
+
+function regression(){
+    currProgress -= 33.3;
+    console.log('curr progress is ' + currProgress);
+    if (Math.round(currProgress) === 0){
+        progressValue.style.height = 0;
+        return;
+      }
+    progressValue.style.height = currProgress +'%';
 }
